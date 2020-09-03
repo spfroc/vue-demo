eaa280cf53caae4d71e79657da22fcc91f5e6f51
spf_roc <spf_roc@163.com>
2020-09-03 09:48:47 +0800

    修改页面内容

--- src/components/mobile/BannerLink.vue
+++ src/components/mobile/BannerLink.vue
@@ -1,36 +1,18 @@
 <template>
-    <mobile-layout></mobile-layout>
+    <div>this is banner link page</div>
 </template>
 
 <script>
-    import MobileLayout from './MobileLayout'
     export default {
         name: "BannerLink",
         components: {
-            MobileLayout
         },
         methods: {
-            connectWebViewJavascriptBridge(callback) {
-                console.log(window.WebViewJavascriptBridge);
-                if (window.WebViewJavascriptBridge) {
-                    callback(WebViewJavascriptBridge)
-                } else {
-                    document.addEventListener('WebViewJavascriptBridgeReady', () => {
-                        callback(WebViewJavascriptBridge)
-                    }, false)
-                }
-            }
+
         },
 
         mounted() {
-            this.connectWebViewJavascriptBridge((bridge) => {
-                bridge.init(function(message, responseCallback) {
-                    log('JS got a message', message)
-                    let data = { 'Javascript Responds':'Wee!' }
-                    log('JS responding with', data)
-                    responseCallback(data)
-                })
-            })
+
         }
     }
 </script>
