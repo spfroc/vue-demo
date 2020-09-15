1f6db5a3b0a78893f8e9c015e352b21e96ebc301
spf_roc <spf_roc@163.com>
2020-09-15 16:23:11 +0800

    社工管理编辑时性别显示错误问题修复

--- src/components/user/SocialWorker.vue
+++ src/components/user/SocialWorker.vue
@@ -175,11 +175,11 @@
                 genderOptions: [
                     {
                         label: '男',
-                        value: 1,
+                        value: "1",
                     },
                     {
                         label: '女',
-                        value: 2,
+                        value: "2",
                     },
                 ],
                 editingRow: {},
