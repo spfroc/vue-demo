b375e792cd2b1ca70ca6ea3ef1efc712aa343838
spf_roc <spf_roc@163.com>
2020-09-17 18:13:09 +0800

    logo更换

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -6,7 +6,7 @@
       <i v-else class="el-icon-s-unfold"></i>
     </div>
     <div class="logo-container">
-      <div class="logo"><img src="../../assets/logo.png" width="35px" alt=""></div>
+      <div class="logo"><img src="../../assets/new_logo.png" width="35px" alt=""></div>
       <span>首善养老管理后台</span>
     </div>
     <div class="header-right">
