5ef09930d65b955fac97df43e851f7d596499b74
spf_roc <spf_roc@163.com>
2020-09-29 23:54:50 +0800

    修改生产环境接口配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   // Mock: false
-  API_HOST: '"wszhylapi.yzsjkj.com.cn"',
-  APP_HOST: '"huberyzhyl.yzsjkj.com.cn"',
+  // API_HOST: '"http://wszhylapi.yzsjkj.com.cn"',
+  // APP_HOST: '"http://huberyzhyl.yzsjkj.com.cn"',
+  API_HOST: '"http://47.104.217.169:8083"',
+  APP_HOST: '"http://47.104.217.169:8084"'
 }
