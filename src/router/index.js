28449fc377391747a101b3f8b6471392234968ed
spf_roc <spf_roc@163.com>
2020-11-05 15:17:03 +0800

    打包优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,99 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Layout from '@/components/layout/Layout'
-import Home from '@/components/Home'
-import Page404 from '@/components/layout/404'
-import Login from '@/components/Login'
-import ServiceBanner from "@/components/app/ServiceBanner";
-import CommunityBanner from "@/components/app/CommunityBanner";
-import Administrator from "@/components/admin/Administrator";
-import CommunityCategory from "../components/app/CommunityCategory";
-import Community from "../components/app/Community";
-import StoreCategory from "../components/store/StoreCategory";
-import Store from "../components/store/Store";
-import Village from "../components/app/Village";
-import StoreComment from "../components/comment/Store";
-import CommunityComment from "../components/comment/Community";
-import ChildrenComment from "../components/comment/Children";
-import Template from "../components/care/Template";
-import Birthday from "../components/care/Birthday";
-import CommonSense from "../components/health/CommonSense";
-import Children from "../components/user/Children";
-import Elders from "../components/user/Elders";
-import ChestCard from "../components/device/ChestCard";
-import ElderFile from "../components/user/ElderFile";
-import Stage from "../components/user/Stage";
-import Doctor from "../components/user/Doctor";
-import SocialWorker from "../components/user/SocialWorker";
-import Chart from "../components/statistics/Chart";
-import Activity from "../components/volunteer/Activity";
-import Approval from "../components/volunteer/Approval";
-import Brain from "../components/city/Brain";
-import ElectronicFence from "../components/device/ElectronicFence";
-import Monitor from "../components/device/Monitor";
-import Center from "../components/call/Center";
-import IndexBanner from "../components/app/IndexBanner";
-import Company from "../components/user/Company";
-import SocialWorkerAssessment from "../components/user/SocialWorkerAssessment";
-import BannerLink from "../components/mobile/BannerLink";
-import Agreement from "../components/mobile/Agreement";
-import CommunityDetail from "../components/mobile/Community";
-import About from "../components/mobile/About";
-import VolunteerDetail from "../components/mobile/VolunteerDetail";
-import HealthDetail from "../components/mobile/HealthDetail";
-import Merchant from "../components/mobile/Merchant";
-import Download from "../components/mobile/Download";
-import CommunityCommentDetail from "../components/mobile/CommunityCommentDetail";
-import Index from "../components/log/Index";
+// import Layout from '@/components/layout/Layout'
+// import Home from '@/components/Home'
+// import Page404 from '@/components/layout/404'
+// import Login from '@/components/Login'
+// import ServiceBanner from "@/components/app/ServiceBanner";
+// import CommunityBanner from "@/components/app/CommunityBanner";
+// import Administrator from "@/components/admin/Administrator";
+// import CommunityCategory from "../components/app/CommunityCategory";
+// import Community from "../components/app/Community";
+// import StoreCategory from "../components/store/StoreCategory";
+// import Store from "../components/store/Store";
+// import Village from "../components/app/Village";
+// import StoreComment from "../components/comment/Store";
+// import CommunityComment from "../components/comment/Community";
+// import ChildrenComment from "../components/comment/Children";
+// import Template from "../components/care/Template";
+// import Birthday from "../components/care/Birthday";
+// import CommonSense from "../components/health/CommonSense";
+// import Children from "../components/user/Children";
+// import Elders from "../components/user/Elders";
+// import ChestCard from "../components/device/ChestCard";
+// import ElderFile from "../components/user/ElderFile";
+// import Stage from "../components/user/Stage";
+// import Doctor from "../components/user/Doctor";
+// import SocialWorker from "../components/user/SocialWorker";
+// import Chart from "../components/statistics/Chart";
+// import Activity from "../components/volunteer/Activity";
+// import Approval from "../components/volunteer/Approval";
+// import Brain from "../components/city/Brain";
+// import ElectronicFence from "../components/device/ElectronicFence";
+// import Monitor from "../components/device/Monitor";
+// import Center from "../components/call/Center";
+// import IndexBanner from "../components/app/IndexBanner";
+// import Company from "../components/user/Company";
+// import SocialWorkerAssessment from "../components/user/SocialWorkerAssessment";
+// import BannerLink from "../components/mobile/BannerLink";
+// import Agreement from "../components/mobile/Agreement";
+// import CommunityDetail from "../components/mobile/Community";
+// import About from "../components/mobile/About";
+// import VolunteerDetail from "../components/mobile/VolunteerDetail";
+// import HealthDetail from "../components/mobile/HealthDetail";
+// import Merchant from "../components/mobile/Merchant";
+// import Download from "../components/mobile/Download";
+// import CommunityCommentDetail from "../components/mobile/CommunityCommentDetail";
+// import Index from "../components/log/Index";
+
+// const Vue = () => import('vue');
+// const Router  = () => import('vue-router')
+const Layout = () => import( '@/components/layout/Layout')
+const Home = () => import( '@/components/Home')
+const Page404 = () => import( '@/components/layout/404')
+const Login = () => import( '@/components/Login')
+const ServiceBanner = () => import( "@/components/app/ServiceBanner");
+const CommunityBanner = () => import( "@/components/app/CommunityBanner");
+const Administrator = () => import( "@/components/admin/Administrator");
+const CommunityCategory = () => import( "../components/app/CommunityCategory");
+const Community = () => import( "../components/app/Community");
+const StoreCategory = () => import( "../components/store/StoreCategory");
+const Store = () => import( "../components/store/Store");
+const Village = () => import( "../components/app/Village");
+const StoreComment = () => import( "../components/comment/Store");
+const CommunityComment = () => import( "../components/comment/Community");
+const ChildrenComment = () => import( "../components/comment/Children");
+const Template = () => import( "../components/care/Template");
+const Birthday = () => import( "../components/care/Birthday");
+const CommonSense = () => import( "../components/health/CommonSense");
+const Children = () => import( "../components/user/Children");
+const Elders = () => import( "../components/user/Elders");
+const ChestCard = () => import( "../components/device/ChestCard");
+const ElderFile = () => import( "../components/user/ElderFile");
+const Stage = () => import( "../components/user/Stage");
+const Doctor = () => import( "../components/user/Doctor");
+const SocialWorker = () => import( "../components/user/SocialWorker");
+const Chart = () => import( "../components/statistics/Chart");
+const Activity = () => import( "../components/volunteer/Activity");
+const Approval = () => import( "../components/volunteer/Approval");
+const Brain = () => import( "../components/city/Brain");
+const ElectronicFence = () => import( "../components/device/ElectronicFence");
+const Monitor = () => import( "../components/device/Monitor");
+const Center = () => import( "../components/call/Center");
+const IndexBanner = () => import( "../components/app/IndexBanner");
+const Company = () => import( "../components/user/Company");
+const SocialWorkerAssessment = () => import( "../components/user/SocialWorkerAssessment");
+const BannerLink = () => import( "../components/mobile/BannerLink");
+const Agreement = () => import( "../components/mobile/Agreement");
+const CommunityDetail = () => import( "../components/mobile/Community");
+const About = () => import( "../components/mobile/About");
+const VolunteerDetail = () => import( "../components/mobile/VolunteerDetail");
+const HealthDetail = () => import( "../components/mobile/HealthDetail");
+const Merchant = () => import( "../components/mobile/Merchant");
+const Download = () => import( "../components/mobile/Download");
+const CommunityCommentDetail = () => import( "../components/mobile/CommunityCommentDetail");
+const Index = () => import( "../components/log/Index");
+
 
 Vue.use(Router)
 
