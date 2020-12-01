19c20ce620c7d435bd15b4e787066fbfa85c69d3
zhangchenglong <zhangchenglong5@xdf.cn>
2020-12-01 19:28:00 +0800

    修改样式

--- src/components/mobile/Community.vue
+++ src/components/mobile/Community.vue
@@ -1,249 +1,293 @@
 <template>
-    <div class="container" style="height: 100%; overflow-y:auto;" v-infinite-scroll="getComments" ref="container">
-        <h4>{{detail.title}}-{{debug}}</h4>
-        <div style="margin-top: 10px;color: gray;">
-            <span>{{dateTimeToDate}}</span>
-        </div>
-        <div class="text-container" v-html="detail.content"></div>
-        <div class="comment-container" style="margin-top: 20px">
-            <el-row style="padding-bottom: 10px">
-                <el-col :span="4">
-                    <div style="font-weight: bold;">评论</div>
-                </el-col>
-                <el-col :span="16">&nbsp;</el-col>
-                <el-col :span="4"><span @click="replyShow(0)" style="color: #fd7f04;">写评价</span></el-col>
-            </el-row>
-            <section v-for="(comment, index) in comments" :key="index" :style="index > 0 ? paddingStyle: ''">
-                <el-row>
-                    <el-col :span="3">
-                        <el-avatar shape="square" :src="'/images'+comment.headImg">
-                            <img src="../../assets/img/default_user.png"/>
-                        </el-avatar>
-                    </el-col>
-                    <el-col :span="13">
-                        <span style="font-weight: bold;">{{comment.userName || 'null'}}</span>
-                    </el-col>
-                </el-row>
-                <el-row>
-                    <el-col><div @click="toDetail(comment.id)">{{comment.content}}</div></el-col>
-                </el-row>
-
-                <el-row v-if="comment.contentImg">
-                    <el-col :span="12"><el-image :src="'/images'+comment.contentImg"></el-image></el-col>
-                </el-row>
-                <el-row style="margin-top: 10px;line-height: 20px;">
-                    <el-col :span="8">
-                        <span><div @click="toDetail(comment.id)" style="color: gray;">{{comment.createTime.substr(5, 11)}}</div></span>
-                    </el-col>
-
-                    <el-col :span="13">
-                        <span><span @click="replyShow(comment.id)">{{comment.replyNum}}回复<i class="el-icon-arrow-right"></i></span></span>
-                    </el-col>
-                    <el-col :span="3">
-                        <span v-if="comment.isMine"><span style="color: red;" @click="deleteComment(comment.id)">删除</span></span>
-                    </el-col>
-
-                </el-row>
-
-            </section>
-            <section style="color: gray;text-align: center;" v-if="page.totalPage <= page.pageNo"><span>没有更多</span></section>
-
-            <!--<comment-dialog v-if="dialogVisible" :form="form" :hide-dialog="hideDialog" :submit="reply" :token="queryParams.token"></comment-dialog>-->
-            <community-comment-detail-dialog v-if="dialogVisible"
-                                             :query-params="queryParams"
-                                             :hide-dialog="hideDialog"
-                                             :evaluate-id="evaluateId"
-                                             ></community-comment-detail-dialog>
-        </div>
-        <section style="color: gray;text-align: center;" v-if="page.totalPage <= page.pageNo"><span>没有更多</span></section>
-
-        <div class="mask" @click="() => {this.dialogVisible = false;}" v-if="dialogVisible"></div>
-
+  <div
+    class="container"
+    style="position:fixed;left:0;top:0;width: 100%;height: 100%; overflow-y:auto;box-sizing: border-box;"
+    v-infinite-scroll="getComments"
+    ref="container"
+  >
+    <h4>{{detail.title}}-{{debug}}</h4>
+    <div style="margin-top: 10px;color: gray;">
+      <span>{{dateTimeToDate}}</span>
     </div>
+    <div
+      class="text-container"
+      v-html="detail.content"
+    ></div>
+    <div
+      class="comment-container"
+      style="margin-top: 20px"
+    >
+      <el-row style="padding-bottom: 10px">
+        <el-col :span="4">
+          <div style="font-weight: bold;">评论</div>
+        </el-col>
+        <el-col :span="16">&nbsp;</el-col>
+        <el-col :span="4"><span
+            @click="replyShow(0)"
+            style="color: #fd7f04;"
+          >写评价</span></el-col>
+      </el-row>
+      <section
+        v-for="(comment, index) in comments"
+        :key="index"
+        :style="index > 0 ? paddingStyle: ''"
+      >
+        <el-row>
+          <el-col :span="3">
+            <el-avatar
+              shape="square"
+              :src="'/images'+comment.headImg"
+            >
+              <img src="../../assets/img/default_user.png" />
+            </el-avatar>
+          </el-col>
+          <el-col :span="13">
+            <span style="font-weight: bold;">{{comment.userName || 'null'}}</span>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col>
+            <div @click="toDetail(comment.id)">{{comment.content}}</div>
+          </el-col>
+        </el-row>
+
+        <el-row v-if="comment.contentImg">
+          <el-col :span="12">
+            <el-image :src="'/images'+comment.contentImg"></el-image>
+          </el-col>
+        </el-row>
+        <el-row style="margin-top: 10px;line-height: 20px;">
+          <el-col :span="8">
+            <span>
+              <div
+                @click="toDetail(comment.id)"
+                style="color: gray;"
+              >{{comment.createTime.substr(5, 11)}}</div>
+            </span>
+          </el-col>
+
+          <el-col :span="13">
+            <span><span @click="replyShow(comment.id)">{{comment.replyNum}}回复<i class="el-icon-arrow-right"></i></span></span>
+          </el-col>
+          <el-col :span="3">
+            <span v-if="comment.isMine"><span
+                style="color: red;"
+                @click="deleteComment(comment.id)"
+              >删除</span></span>
+          </el-col>
+
+        </el-row>
+
+      </section>
+      <section
+        style="color: gray;text-align: center;"
+        v-if="page.totalPage <= page.pageNo"
+      ><span>没有更多</span></section>
+
+      <!--<comment-dialog v-if="dialogVisible" :form="form" :hide-dialog="hideDialog" :submit="reply" :token="queryParams.token"></comment-dialog>-->
+      <community-comment-detail-dialog
+        v-if="dialogVisible"
+        :query-params="queryParams"
+        :hide-dialog="hideDialog"
+        :evaluate-id="evaluateId"
+      ></community-comment-detail-dialog>
+    </div>
+    <section
+      style="color: gray;text-align: center;"
+      v-if="page.totalPage <= page.pageNo"
+    ><span>没有更多</span></section>
+
+    <div
+      class="mask"
+      @click="() => {this.dialogVisible = false;}"
+      v-if="dialogVisible"
+    ></div>
+
+  </div>
 </template>
 
 <script>
-    import CommunityCommentDetailDialog from './CommunityCommentDetailDialog'
-    export default {
-        name: "Community",
-        components: {
-            CommunityCommentDetailDialog
-        },
-        data() {
-            return {
-                rules: {
-                    content: {required: true, message: '请输入评论内容', trigger: 'blur'}
-                },
-                form: {
-                    content: ''
-                },
-                debug: 'debug',
-                dialogVisible: false,
-                detail: {},
-                queryParams: {},
-                paddingStyle: {
-                  paddingTop: '20px'
-                },
-                page:{
-                    pageNo: 1,
-                    pageSize: 10,
-                    totalPage: 10
-                },
-                comments: [],
-                evaluateId: 0
-            }
-        },
-
-        computed:{
-            dateTimeToDate() {
-                if(this.detail.createTime) {
-                    if(isNaN(this.detail.createTime)) {
-                        return this.detail.createTime.substr(0,10)
-
-                    } else {
-                        let date = new Date(this.detail.createTime)
-                        return date.getFullYear() + '-' + (parseInt(date.getMonth())+1) + '-' + date.getDate()
-                    }
-                } else {
-                    return ''
-                }
-            }
-        },
-
-        methods: {
-            hideDialog() {
-                this.dialogVisible = false;
-            },
-            toDetail(id) {
-                window.location.href = '/#/app/community-comment-detail?evaluateId='+id + '&communityId='+ this.queryParams.id +'&token='+this.queryParams.token;
-            },
-            replyShow(evaluateId = 0) {
-                if(evaluateId != 0) {
-                    this.evaluateId = evaluateId;
-                }
-                this.dialogVisible = true;
-            },
-
-            reply() {
-                this.$refs['form'].validate((valid) => {
-                    if(valid) {
-                        this.form.token = this.queryParams.token;
-                        this.form.communityId = this.queryParams.id;
-                        this.$common.appTokenAxios().get('/app/community/evaluate', {
-                            params: this.form
-                        }).then(res => {
-                            console.log(res);
-                            this.$message({
-                                message: res.data.message || '评价成功！将在审核通过后显示',
-                                type: 'success'
-                            })
-                            this.dialogVisible = false;
-                        })
-                    }
-
-                })
-            },
-
-            deleteComment(id) {
-                this.removeCommentFromCommentsList(id)
-                this.$common.appTokenAxios().get('/app/mine/deleteEvaluate', {
-                    params: {
-                        id: id,
-                        type: 2,
-                        token: this.queryParams.token,
-                    }
-                }).then(res => {
-                    this.$message({
-                        message: res.data.message || '操作成功',
-                        type: 'success'
-                    })
-                })
-
-            },
-
-            removeCommentFromCommentsList(id) {
-                this.comments.forEach((comment, index) => {
-                    if(id == comment.id) {
-                        this.comments.splice(index, 1)
-                        return;
-                    }
-                })
-            },
-
-            getDetail() {
-                this.$common.appTokenAxios().get('/app/community/detail?id='+this.queryParams.id, {
-                    params: {
-                        token: this.queryParams.token,
-                    }
-                }).then((res) => {
-                    this.detail = res.data.data;
-                });
-            },
-
-            getComments(currentPage) {
-                if(currentPage) {
-                    this.page.pageNo = currentPage;
-                }
-                console.log(this.page.pageNo, this.page.totalPage, this.page.pageNo <= this.page.totalPage);
-
-                if(this.page.pageNo <= this.page.totalPage) {
-                    this.$common.appTokenAxios().get('/app/community/evaluateList', {
-                        params: {
-                            token: this.queryParams.token,
-                            communityId: this.queryParams.id,
-                            pageNo: this.page.pageNo,
-                            pageSize: this.page.pageSize
-                        }
-                    }).then(res => {
-                        this.comments = this.comments.concat(res.data.data.list);
-                        this.page.totalPage = res.data.data.totalPage;
-                        this.page.pageNo ++;
-                    });
-                }
-
-            },
-
-        },
-
-        mounted() {
-            this.queryParams = this.$route.query;
-            this.getDetail();
-            // this.getComments(1);
-        }
+import CommunityCommentDetailDialog from './CommunityCommentDetailDialog'
+export default {
+  name: "Community",
+  components: {
+    CommunityCommentDetailDialog
+  },
+  data () {
+    return {
+      rules: {
+        content: { required: true, message: '请输入评论内容', trigger: 'blur' }
+      },
+      form: {
+        content: ''
+      },
+      debug: 'debug',
+      dialogVisible: false,
+      detail: {},
+      queryParams: {},
+      paddingStyle: {
+        paddingTop: '20px'
+      },
+      page: {
+        pageNo: 1,
+        pageSize: 10,
+        totalPage: 10
+      },
+      comments: [],
+      evaluateId: 0
     }
+  },
+
+  computed: {
+    dateTimeToDate () {
+      if (this.detail.createTime) {
+        if (isNaN(this.detail.createTime)) {
+          return this.detail.createTime.substr(0, 10)
+
+        } else {
+          let date = new Date(this.detail.createTime)
+          return date.getFullYear() + '-' + (parseInt(date.getMonth()) + 1) + '-' + date.getDate()
+        }
+      } else {
+        return ''
+      }
+    }
+  },
+
+  methods: {
+    hideDialog () {
+      this.dialogVisible = false;
+    },
+    toDetail (id) {
+      window.location.href = '/#/app/community-comment-detail?evaluateId=' + id + '&communityId=' + this.queryParams.id + '&token=' + this.queryParams.token;
+    },
+    replyShow (evaluateId = 0) {
+      if (evaluateId != 0) {
+        this.evaluateId = evaluateId;
+      }
+      this.dialogVisible = true;
+    },
+
+    reply () {
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          this.form.token = this.queryParams.token;
+          this.form.communityId = this.queryParams.id;
+          this.$common.appTokenAxios().get('/app/community/evaluate', {
+            params: this.form
+          }).then(res => {
+            console.log(res);
+            this.$message({
+              message: res.data.message || '评价成功！将在审核通过后显示',
+              type: 'success'
+            })
+            this.dialogVisible = false;
+          })
+        }
+
+      })
+    },
+
+    deleteComment (id) {
+      this.removeCommentFromCommentsList(id)
+      this.$common.appTokenAxios().get('/app/mine/deleteEvaluate', {
+        params: {
+          id: id,
+          type: 2,
+          token: this.queryParams.token,
+        }
+      }).then(res => {
+        this.$message({
+          message: res.data.message || '操作成功',
+          type: 'success'
+        })
+      })
+
+    },
+
+    removeCommentFromCommentsList (id) {
+      this.comments.forEach((comment, index) => {
+        if (id == comment.id) {
+          this.comments.splice(index, 1)
+          return;
+        }
+      })
+    },
+
+    getDetail () {
+      this.$common.appTokenAxios().get('/app/community/detail?id=' + this.queryParams.id, {
+        params: {
+          token: this.queryParams.token,
+        }
+      }).then((res) => {
+        this.detail = res.data.data;
+      });
+    },
+
+    getComments (currentPage) {
+      if (currentPage) {
+        this.page.pageNo = currentPage;
+      }
+      console.log(this.page.pageNo, this.page.totalPage, this.page.pageNo <= this.page.totalPage);
+
+      if (this.page.pageNo <= this.page.totalPage) {
+        this.$common.appTokenAxios().get('/app/community/evaluateList', {
+          params: {
+            token: this.queryParams.token,
+            communityId: this.queryParams.id,
+            pageNo: this.page.pageNo,
+            pageSize: this.page.pageSize
+          }
+        }).then(res => {
+          this.comments = this.comments.concat(res.data.data.list);
+          this.page.totalPage = res.data.data.totalPage;
+          this.page.pageNo++;
+        });
+      }
+
+    },
+
+  },
+
+  mounted () {
+    this.queryParams = this.$route.query;
+    this.getDetail();
+    // this.getComments(1);
+  }
+}
 </script>
 
 <style>
 .text-container {
-    /*padding-top: 10px;*/
+  /*padding-top: 10px;*/
 }
 .el-dialog__header {
-    display: none;
+  display: none;
 }
 .el-dialog__footer {
-    display: none;
+  display: none;
 }
 .el-dialog__body {
-    padding: 0 0;
+  padding: 0 0;
 }
 
 textarea {
-    min-height: 54px;
-    height: 148px;
+  min-height: 54px;
+  height: 148px;
 }
 .el-textarea__inner {
-    padding: 0px 0px 0px 20px;
+  padding: 0px 0px 0px 20px;
 }
 .el-dialog__wrapper {
-    overflow: visible;
+  overflow: visible;
 }
 
-.mask{
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    z-index: 2999;
-    top: 0px;
-    left: 0px;
+.mask {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 2999;
+  top: 0px;
+  left: 0px;
 }
 </style>
--- src/components/mobile/CommunityCommentDetailDialog.vue
+++ src/components/mobile/CommunityCommentDetailDialog.vue
@@ -1,114 +1,129 @@
 <template>
-    <div class="comment-dialog" ref="dom-to-scroll">
-        <el-row>
-            <el-col :span="20">
-                <el-form :model="form" :rules="rules" ref="form">
-                    <el-form-item prop="content">
-                        <el-input style="width: 100%;"
-                                  type="textarea"
-                                  :placeholder='"评价内容~~" + debug'
-                                  @focus="scrollToViewLocation"
-                                  :autosize="{ minRows: 2, maxRows: 5}"
-                                  v-model="form.content"></el-input>
-                    </el-form-item>
-                </el-form>
+  <div
+    class="comment-dialog"
+    ref="dom-to-scroll"
+  >
+    <el-row>
+      <el-col :span="20">
+        <el-form
+          :model="form"
+          :rules="rules"
+          ref="form"
+        >
+          <el-form-item prop="content">
+            <el-input
+              style="width: 100%;"
+              type="textarea"
+              :placeholder='"评价内容~~" + debug'
+              @focus="scrollToViewLocation"
+              :autosize="{ minRows: 2, maxRows: 5}"
+              v-model="form.content"
+            ></el-input>
+          </el-form-item>
+        </el-form>
 
-            </el-col>
-            <el-col :span="4">
-                <span style="color: #fd7f04;font-size: 20px;margin-left: 15px;font-weight: bold;" @click="reply">发布</span>
-            </el-col>
-        </el-row>
-        <span slot="footer" class="dialog-footer"></span>
-    </div>
+      </el-col>
+      <el-col :span="4">
+        <span
+          style="color: #fd7f04;font-size: 20px;margin-left: 15px;font-weight: bold;"
+          @click="reply"
+        >发布</span>
+      </el-col>
+    </el-row>
+    <span
+      slot="footer"
+      class="dialog-footer"
+    ></span>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "CommunityCommentDetailDialog",
+export default {
+  name: "CommunityCommentDetailDialog",
 
-        props: [
-            "queryParams", "hideDialog","evaluateId","communityId"
-        ],
+  props: [
+    "queryParams", "hideDialog", "evaluateId", "communityId"
+  ],
 
-        data() {
-            return {
-                debug: 'debug',
-                rules: {
-                    content: {required: true, message: '请输入评论内容', trigger: 'blur'}
-                },
-                form: {
-                    content: ''
-                },
-            }
-        },
-
-        methods: {
-            reply() {
-                this.$refs['form'].validate((valid) => {
-                    if(valid) {
-                        this.form.token = this.queryParams.token;
-                        if(this.queryParams.id) {
-                            this.form.communityId = this.queryParams.id;
-                        } else if(this.queryParams.communityId) {
-                            this.form.communityId = this.queryParams.communityId;
-                        }
-
-                        if(this.evaluateId) {
-                            this.form.evaluateId = this.evaluateId
-                            this.form.subEvaluateId = this.evaluateId
-                        }
-                        this.$common.appTokenAxios().get('/app/community/evaluate', {
-                            params: this.form
-                        }).then(res => {
-                            console.log(res);
-                            this.$message({
-                                message: res.data.message || '评价成功！将在审核通过后显示',
-                                type: 'success'
-                            })
-                            this.hideDialog()
-                        })
-                    }
-
-                })
-            },
-
-            scrollToViewLocation() {
-                setTimeout(() => {
-                    this.$refs['dom-to-scroll'].scrollIntoView({behavior: 'smooth', block: 'start'});
-                }, 500)
-            }
-        },
-
-        mounted() {
-            let ua = window.navigator.userAgent.toLocaleLowerCase();
-
-            console.log(ua);
-            const originHeight = document.documentElement.clientHeight || document.body.clientHeight;
-            window.addEventListener('resize', () => {
-                const resizeHeight = document.documentElement.clientHeight || document.body.clientHeight;
-                if (resizeHeight < originHeight) {
-                    // 键盘弹起所后所需的页面逻辑
-                    this.debug = '1';
-                    this.scrollToViewLocation();
-                } else {
-                    // 键盘弹起所后所需的页面逻辑
-                    this.debug = '2';
-                    this.scrollToViewLocation();
-                }
-            }, false);
-
-
-        }
+  data () {
+    return {
+      debug: 'debug',
+      rules: {
+        content: { required: true, message: '请输入评论内容', trigger: 'blur' }
+      },
+      form: {
+        content: ''
+      },
     }
+  },
+
+  methods: {
+    reply () {
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          this.form.token = this.queryParams.token;
+          if (this.queryParams.id) {
+            this.form.communityId = this.queryParams.id;
+          } else if (this.queryParams.communityId) {
+            this.form.communityId = this.queryParams.communityId;
+          }
+
+          if (this.evaluateId) {
+            this.form.evaluateId = this.evaluateId
+            this.form.subEvaluateId = this.evaluateId
+          }
+          this.$common.appTokenAxios().get('/app/community/evaluate', {
+            params: this.form
+          }).then(res => {
+            console.log(res);
+            this.$message({
+              message: res.data.message || '评价成功！将在审核通过后显示',
+              type: 'success'
+            })
+            this.hideDialog()
+          })
+        }
+
+      })
+    },
+
+    scrollToViewLocation () {
+      // setTimeout(() => {
+      //     this.$refs['dom-to-scroll'].scrollIntoView({behavior: 'smooth', block: 'start'});
+      // }, 500)
+    }
+  },
+
+  mounted () {
+    let ua = window.navigator.userAgent.toLocaleLowerCase();
+
+    console.log(ua);
+    // const originHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    // window.addEventListener('resize', () => {
+    //   const resizeHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    //   if (resizeHeight < originHeight) {
+    //     // 键盘弹起所后所需的页面逻辑
+    //     this.debug = '1';
+    //     this.scrollToViewLocation();
+    //   } else {
+    //     // 键盘弹起所后所需的页面逻辑
+    //     this.debug = '2';
+    //     this.scrollToViewLocation();
+    //   }
+    // }, false);
+
+
+  }
+}
 </script>
 
 <style scoped>
-    .comment-dialog {
-        position: fixed;
-        bottom: 0px;
-        background-color: white;
-        width: 94%;
-        height: 120px;
-        z-index: 3000;
-    }
+.comment-dialog {
+  position: fixed;
+  bottom: 0px;
+  background-color: white;
+  width: 94%;
+  height: 120px;
+  z-index: 3000;
+}
 </style>
